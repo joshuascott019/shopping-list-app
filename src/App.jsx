6fd1f706ffa21b61861f2867c0354c90ba106507
Joshua Scott <joshua.ds.s019@gmail.com>
2025-12-19 15:11:04 -0300

    feat: wire BudgetBar into TripPage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import TripPage from './pages/TripPage';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<TripPage />} />
+      </Routes>
+    </Router>
+  );
 };
+
 export default App;
---
+++ src/pages/TripPage.jsx
@@ -0,0 +1,10 @@
+import BudgetBar from '@/components/BudgetBar';
+
+const TripPage = () => {
+  return (
+    <div>
+      <BudgetBar />
+    </div>
+  );
+};
+export default TripPage;
